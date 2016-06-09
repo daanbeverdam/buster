0960ee97b57c9f03fda758a7b604652e68539edd
Daan Beverdam <daanbeverdam@gmail.com>
2016-06-10 01:13:47 +0200

    added top right info card

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,17 @@
 function toggle_menu () {
     menu = document.getElementById('sidebar');
+    sunscreen = document.getElementById('sunscreen');
     if (menu.style.display == 'none' || menu.style.display === '') {
         menu.style.display = 'block';
         menu.style.padding = 0;
         menu.style.position = 'fixed';
+        sunscreen.style.display = 'block';
     } else {
         menu.style.display = null;
         menu.style.position = null;
         menu.style.padding = null;
         menu.style.paddingRight = null;
+        sunscreen.style.display = null;
     }
 }
 
