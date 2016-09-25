edf8ac0cb39070c5046e2cc16d8a5f0bfb90550f
Daan Beverdam <daanbeverdam@gmail.com>
2016-09-25 18:49:55 +0200

    switched to velocity
    Due to poor performance on mobile, I switched to Velocity.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,30 @@
 function toggle_sidebar () {
-    menu = document.getElementById('sidebar');
-    if (menu.style.top == '150%' || menu.style.top === '') {
-        $('#sunscreen').fadeIn('slow');
-        $('#sidebar').animate({'top': '50%'}, 500);
+    infocard = document.getElementById('sidebar');
+    sunscreen = document.getElementById('sunscreen');
+    if (infocard.style.top == '150%' || infocard.style.top === '') {
+        sunscreen.style.display = 'block';
+        Velocity(sunscreen, {opacity: 0.25}, 250);
+        Velocity(infocard, {'top': '50%'}, 500);
     } else {
-        $('#sunscreen').fadeOut('slow');
-        $('#sidebar').animate({'top': '150%'}, 500);
+        Velocity(sunscreen, {opacity: 0.25}, 250);
+        Velocity(infocard, {'top': '150%'}, 500);
+        sunscreen.style.display = 'none'; // todo: put in callback
     }
 }
 
 function toggle_navigation () {
-    if ($('#content').css('left') === '150px') {
-        $('#content').animate({'left': '0px'}, 250);
-        $('body').css('overflow', 'initial');
-        $('body').css('position', 'relative');
+    body = document.body;
+    header = document.getElementById('header');
+    content = document.getElementById('content');
+    if (content.style.left === '150px') {
+        Velocity(content, {'left': '0px'}, 250);
+        //Velocity(header, {'left': '0px'}, 250); // Firefox and Safari made me do this
+        body.style.overflow = 'initial';
+        body.style.position = 'relative';
     } else {
-        $('#content').animate({'left': '150px'}, 250);
-        $('body').css('overflow', 'hidden');
-        $('body').css('position', 'fixed');
+        Velocity(content, {'left': '150px'}, 250);
+        //Velocity(header, {'left': '150px'}, 250); // Firefox and Safari made me do this
+        body.style.overflow = 'hidden';
+        body.style.position = 'fixed';
     }
-}
-
-function scroll_to_top() {
-    $('html, body').animate({ scrollTop: 0 }, 'fast');
 }
