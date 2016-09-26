fbf14a66c6b9aef6b7fd9009a911d7bcba329ca4
Daan Beverdam <daanbeverdam@gmail.com>
2016-09-26 11:35:36 +0200

    removed sticky header
    Why: because of animation/scroll problems on mobile.

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,12 +16,12 @@ function toggle_navigation () {
     content = document.getElementById('content');
     if (content.style.left === '150px') {
         Velocity(content, {'left': '0px'}, 250);
-        //Velocity(header, {'left': '0px'}, 250); // Firefox and Safari made me do this
+        // Velocity(header, {'left': '0px'}, 250); // Firefox and Safari made me do this
         body.style.overflow = 'initial';
         body.style.position = 'relative';
     } else {
         Velocity(content, {'left': '150px'}, 250);
-        //Velocity(header, {'left': '150px'}, 250); // Firefox and Safari made me do this
+        // Velocity(header, {'left': '150px'}, 250); // Firefox and Safari made me do this
         body.style.overflow = 'hidden';
         body.style.position = 'fixed';
     }
