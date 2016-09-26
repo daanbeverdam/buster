72a5020e1593ba21c9f8e2ef9f3751667184b590
Daan Beverdam <daanbeverdam@gmail.com>
2016-09-26 14:24:58 +0200

    polished animations

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,15 @@
+/*jshint esversion: 6 */
+var showing_sidebar = false;
+var showing_navigation = false;
+
 function toggle_sidebar () {
     infocard = document.getElementById('sidebar');
-    sunscreen = document.getElementById('sunscreen');
     if (infocard.style.top == '150%' || infocard.style.top === '') {
-        Velocity(sunscreen, {opacity: 0.25}, {display: 'block'});
+        showing_sidebar = true;
         Velocity(infocard, {'top': '50%'}, 600);
+        show_sunscreen();
     } else {
-        Velocity(sunscreen, {opacity: 0}, {display: 'none'});
+        showing_sidebar = false;
         Velocity(infocard, {'top': '150%'}, 600);
     }
 }
@@ -15,14 +19,42 @@ function toggle_navigation () {
     header = document.getElementById('header');
     content = document.getElementById('content');
     if (content.style.left === '150px') {
+        showing_navigation = false;
         Velocity(content, {'left': '0px'}, 250);
-        // Velocity(header, {'left': '0px'}, 250); // Firefox and Safari made me do this
         body.style.overflow = 'initial';
         body.style.position = 'relative';
     } else {
+        showing_navigation = true;
         Velocity(content, {'left': '150px'}, 250);
-        // Velocity(header, {'left': '150px'}, 250); // Firefox and Safari made me do this
+        show_sunscreen(true, 0);
         body.style.overflow = 'hidden';
         body.style.position = 'fixed';
     }
+}
+
+function show_sunscreen (bool=true, opacity=0.25) {
+    sunscreen = document.getElementById('sunscreen');
+    if (bool) {
+        Velocity(sunscreen, {opacity: opacity}, {display: 'block'});
+        if (showing_navigation) {
+            sunscreen.style.left = '150px';
+        }
+    } else {
+        if (sunscreen.style.left === '150px') {
+            sunscreen.style.left = '0px';
+        }
+        Velocity(sunscreen, {opacity: 0}, {display: 'none'});
+    }
+}
+
+function clear_sunscreen () {
+    if (showing_sidebar && showing_navigation) {
+        toggle_sidebar();
+        toggle_navigation();
+    } else if (showing_sidebar) {
+        toggle_sidebar();
+    } else {
+        toggle_navigation();
+    }
+    show_sunscreen(false);
 }
