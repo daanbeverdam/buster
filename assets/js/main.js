94ee5ac5695be1ab368eba9bc943afba8bfd47e0
Daan Beverdam <daanbeverdam@gmail.com>
2016-10-26 01:00:08 +0200

    new menu for mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,19 +15,14 @@ function toggle_sidebar () {
 
 function toggle_navigation () {
     body = document.body;
-    header = document.getElementById('header');
-    content = document.getElementById('content');
-    if (content.style.left === '150px') {
-        showing_navigation = false;
-        Velocity(content, {'left': '0px'}, 250);
-        body.style.overflow = 'initial';
-        body.style.position = 'relative';
-    } else {
+    navigation = document.getElementById('navigation');
+    if (navigation.style.left === '-160px' || navigation.style.left === '') {
         showing_navigation = true;
-        Velocity(content, {'left': '150px'}, 250);
-        show_sunscreen(true, 0);
-        body.style.overflow = 'hidden';
-        body.style.position = 'fixed';
+        show_sunscreen(true, 0.25);
+        Velocity(navigation, {'left': '0px'}, 250);
+    } else {
+        showing_navigation = false;
+        Velocity(navigation, {'left': '-160px'}, 250);
     }
 }
 
@@ -35,13 +30,7 @@ function show_sunscreen (bool, opacity) {
     sunscreen = document.getElementById('sunscreen');
     if (bool) {
         Velocity(sunscreen, {opacity: opacity}, {display: 'block'});
-        if (showing_navigation) {
-            sunscreen.style.left = '150px';
-        }
     } else {
-        if (sunscreen.style.left === '150px') {
-            sunscreen.style.left = '0px';
-        }
         Velocity(sunscreen, {opacity: 0}, {display: 'none'});
     }
 }
